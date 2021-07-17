c2201187aae14040f0b430e1db9323a41e9ca976
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2021-07-17 20:08:40 +0200

    Affichage liste article + affichage détail article

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/article/{id}', name: 'article')]
+    public function article($id): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $article = $repo->find($id);
+        if(!$article){
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('article/detailArticle.html.twig', [
+            'article' => $article,
         ]);
     }
 }
