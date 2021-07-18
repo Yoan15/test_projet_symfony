fd41c953266a777c6f8461aefaabe4c4f6f9576e
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2021-07-18 16:30:20 +0200

    affichage des catégories et des articles correspondant + création formulaire inscription

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,18 +13,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     private $repoArticle;
+    private $repoCategory;
 
-    public function __construct(ArticleRepository $repoArticle)
+    public function __construct(ArticleRepository $repoArticle, 
+                                CategoryRepository $repoCategory)
     {
         $this->repoArticle = $repoArticle;
+        $this->repoCategory = $repoCategory;
     }
 
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $categories = $this->repoCategory->findAll();
         $articles = $this->repoArticle->findAll();
         return $this->render('home/index.html.twig', [
             'articles' => $articles,
+            'categories' => $categories,
         ]);
     }
 
@@ -34,6 +41,21 @@ class HomeController extends AbstractController
         }
         return $this->render('article/detailArticle.html.twig', [
             'article' => $article,
+        ]);
+    }
+
+    #[Route('/articlesByCategory/{id}', name: 'articles_category')]
+    public function articlesByCategory(?Category $category): Response
+    {
+        if($category){
+            $articles = $category->getArticles()->getValues();
+        }else{
+            return $this->redirectToRoute('home');
+        }
+        $categories = $this->repoCategory->findAll();
+        return $this->render('home/index.html.twig', [
+            'articles' => $articles,
+            'categories' => $categories,
         ]);
     }
 }
